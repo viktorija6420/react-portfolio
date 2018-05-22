8ef98d70c2288d3a9eeac86502196d4e09fe40ea
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2018-05-22 16:42:24 -0400

    added fresh info

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -16,33 +16,33 @@ class Home extends Component {
       <div>
         <Container/>
         <h1>Viktorija Gjorgjievska</h1>
-        <Image src={require('../assets/rsz_smartviki.jpg')} alt="whatever"/>
+        <Image src={require('../assets/resume-picture.jpg')} alt="resume-picture"/>
 
         <p>
          <strong>My story</strong>
         </p>
 
         <p>
-        I love coding even though it fucking sucks all the time.
+        I started Web Development as a journey to expand the limits of my intellectual capabilities and explore my visual conceptualisation. I want to believe that human potential is limitless as long as there is enough focus put into its development. I see myself as creative and passionate for challenges. I want to be the bridge between art and logic in the world of business.
         </p>
 
         <p>
-         <strong>Things I have made</strong>
+         <strong>Projects</strong>
         </p>
-
-        <p>
-         Links of websites and stuff i have made
-        </p>
-
-        <p>
-        <strong>What I am currently working on</strong>
-        </p>
-
-        <p>Link from a gitHub repo</p>
+        <ul>
+          <li><a href="https://github.com/viktorija6420">GitHub Account</a></li>
+          <li><a href="https://easybreezymove.herokuapp.com/">Mover App</a></li>
+          <li><a href="https://ser-estar.herokuapp.com/index.html">Quiz for Spanish Practice</a></li>
+          <li><a href="https://www.languagecognition.ca/">Website for Language Tutoring</a></li>
+          <li><a href="http://vip-travel-shop.com.preview.services/#">Travel Agency Static Page with Anchor Links</a></li>
+          <li><a href="https://blooming-temple-73019.herokuapp.com/categories">Kaizen-  connecting tutors with locals.</a></li>
+        </ul>
 
         <p>
           <strong>Technologies</strong>
          </p>
+
+
 
         <ul>
           <li>Languages</li>
@@ -53,33 +53,23 @@ class Home extends Component {
         </ul>
 
         <p><strong>Education</strong></p>
-         <p>General Assembly Bitmaker</p>
-         <p>Full-Stack Web Development</p>
+         <p>Bachelor of Arts in Translation and Interpreting</p>
+         <h3> SAINTS CYRIL AND METHODIUS UNIVERSITY OF SKOPJE</h3>
+         <p> Confirmed as <strong>recognised university in Canada</strong> by Comparative Education Service at the University of Toronto School of Continuing Studies</p>
+         <p> Skopje, Macedonia |  2009 - 2013</p>
          <p>August-November 2017</p>
 
-        <p><strong>Self-thought in:</strong></p>
-        <ul>
-          <li>ES6</li>
-          <li>JSX</li>
-          <li>React</li>
-        </ul>
 
-
-        <p><strong>Things I do when I am not coding</strong></p>
+        <p><strong>Interests</strong></p>
 
         <ul>
-          <li>Long walks in nature</li>
-          <li>Pilates</li>
-          <li>Watching a lot of TV shows to catch up with the world</li>
-          <li>Learning some new spoken language</li>
-          <li>Talking to my friends from around the Globe</li>
-          <li>Watching UFC fights</li>
-          <li>Reading on some new theory about the meaning of life</li>
-          <li>Planning my next trip to somewhere</li>
+          <li>Technology</li>
+          <li>Learning</li>
+          <li>Hiking</li>
+          <li>Spoken Languages</li>
         </ul>
 
         <p><strong>My life before coding</strong></p>
-        <p>Bachelor of Arts in Translation and Interpreting</p>
         <p>Translation jobs</p>
     </div>
     );
